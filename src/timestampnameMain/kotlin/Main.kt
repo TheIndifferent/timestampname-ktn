38fbdbf6ce5214b784960997ee761001ca4c847a
Stanislav Baiduzhyi <baiduzhyi.devel@gmail.com>
2019-01-11 23:09:16 +0100

    Experimenting with the project structure.

--- src/timestampnameMain/kotlin/Main.kt
+++ src/timestampnameMain/kotlin/Main.kt
@@ -1,23 +1,10 @@
-/*
- * Copyright 2010-2018 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package sample.csvparser
+package io.github.theindifferent.timestampname
 
 import kotlinx.cinterop.*
 import platform.posix.*
+
+fun main(args: Array<String>) {
+}
 
 fun parseLine(line: String, separator: Char) : List<String> {
     val result = mutableListOf<String>()
@@ -40,7 +27,7 @@ fun parseLine(line: String, separator: Char) : List<String> {
     return result
 }
 
-fun main(args: Array<String>) {
+fun _main(args: Array<String>) {
     if (args.size != 3) {
         println("Usage: csvparser.kexe <file.csv> <column> <count>")
         return
